783a9db29c3761221f140f7dabb77c30621640f6
Steven Zinck <szinck@gmail.com>
2018-05-08 11:49:28 -0300

    Comment out storage_account_id since it only works if Sku is set to Classic

--- main.tf
+++ main.tf
@@ -17,5 +17,6 @@ resource "azurerm_container_registry" "cr" {
   location            = "${azurerm_resource_group.rg.location}"
   admin_enabled       = true
   sku                 = "${var.sku}"
-  storage_account_id  = "${azurerm_storage_account.rg.id}"
+# only works if Sku = Classic
+#  storage_account_id  = "${azurerm_storage_account.rg.id}"
 }
